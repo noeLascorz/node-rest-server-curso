3338a2c400b9e1e694af357e75617a02988726f5
Noe <noeliasebastian@gmail.com>
2019-07-14 19:40:41 +0200

    Variable entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://noe:8GOUWrQE5PqnyZzm@mflix-gjykd.mongodb.net/test?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
