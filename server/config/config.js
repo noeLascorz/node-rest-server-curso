d9a1ea0657eefb2f8b85ac12d8ffcdf8fbd892ca
Noe <noeliasebastian@gmail.com>
2019-07-14 18:55:46 +0200

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 // Puerto
-process.env.PORT = process.env.PORT || 3000
+process.env.PORT = process.env.PORT || 3001
+
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// Base de datos
+let urlDB
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://noe:8GOUWrQE5PqnyZzm@mflix-gjykd.mongodb.net/test?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB
